e12ad1cd431dbf08a8766f2acdb0564081815eab
Ezra Casas <ezra.casas@startmail.com>
2022-09-21 07:30:14 -0600

    Initial Commit

---
+++ Flashcard UltraUITests/Flashcard_UltraUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flashcard_UltraUITestsLaunchTests.swift
+//  Flashcard UltraUITests
+//
+//  Created by Ezra Casas on 9/21/22.
+//
+
+import XCTest
+
+final class Flashcard_UltraUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
