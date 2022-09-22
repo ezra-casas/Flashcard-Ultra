12d2bd99b2b6f64c816bccfe2aa1dfc9952f5564
Ezra Casas <ezra.casas@startmail.com>
2022-09-22 09:01:46 -0600

    added visibility of question and answer cards.

--- Flashcard Ultra/ViewController.swift
+++ Flashcard Ultra/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapCard(_ sender: Any) {
+        questionLabel.isHidden = !questionLabel.isHidden;
+        answeLabel.isHidden = !answeLabel.isHidden
+    }
+    
 
 }
 
